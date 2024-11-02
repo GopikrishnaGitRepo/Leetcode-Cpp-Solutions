911f97cf92bd45354ec0ba6260d2fbf0e708c4c6
GopikrishnaGitRepo <gopikrishnara4@gmail.com>
2024-11-02 23:24:23 +0530

    solved twosums problem

--- twosums.cpp
+++ twosums.cpp
@@ -1,9 +1,32 @@
 #include<iostream>
 #include<unordered_map>
+#include<vector>
 using namespace std;
+
+// LeetCode
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < size(nums); i++) {
+            for (int j = i + 1; j < size(nums); j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
 int main(void){
 
-    
+    Solution sol;
+    vector<int> input = {2,7,11,15}, result;
+    int tofind = 9;
+    result = sol.twoSum(input, tofind);
+    if ( !result.empty() ){
+        cout << "index " << result[0] << " " << result[1] << endl;
+    }
 
     /*  To revise the working of unordered_map 
         Unordered map stores the value as key:value pair 
@@ -15,12 +38,14 @@ int main(void){
             it->first , to get the key
             it->second , to get the value
     */ 
-    unordered_map<int, int> test;
+    /* unordered_map<int, int> test;
     for (int i=0; i<3; i++){
         test[i] = i+1;
     }
     for ( auto x : test){
         cout << x.first << " " << x.second << endl;
     }
+    */
     return 0;
+    
 }
