819507eec469ef3794615c018b4e7842d469c760
GopikrishnaGitRepo <gopikrishnara4@gmail.com>
2024-11-03 21:45:35 +0530

    added medianoftwosortedarrays.cpp

---
+++ medianoftwosortedarrays.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
+int main(void){
+    cout << "Median of Two Sorted Arrays" << endl;
+    vector<int> v1 = {1,3};
+    vector<int> v2 = {2};
+    median = findMedianSortedArrays(v1,v2);
+    cout << "Median is " << median << endl;
+    return 0;
+}
